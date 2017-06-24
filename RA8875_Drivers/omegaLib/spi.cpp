9ac5a4639b541a3a29498b2fcd3953ec9d97ce56
Oxmose <alexy.torresa@gmail.com>
2017-06-24 15:47:45 +0200

    Ported / NON TESTES SPI interface management
    Added SPI class and functionnalities

---
+++ RA8875_Drivers/omegaLib/spi.cpp
@@ -0,0 +1,114 @@
+/* 
+ * SPI class for Omega2
+ *
+ * Allow compatibility with Mbed code
+ * Not all the functionnalities have been ported,
+ * just the ones used in the project.
+ * 
+ * Author : Alexy Torres Aurora Dugo
+ */
+
+#include "spi.h"
+#include <iostream>
+
+#define PIN_INIT    0x01
+#define FORMAT_INIT 0x02
+#define FREQ_INIT   0x04
+
+#define COMPLETE_INIT 0x07
+
+SPI::SPI(PinName mosi, PinName miso, PinName sclk, PinName ssel = NC_PIN)
+{
+	/* Init members */
+	this->mosi = mosi;
+	this->miso = miso;
+	this->sclk = sclk;
+	this->ssel = ssel;
+
+	this->initSeq = PIN_INIT;
+}
+SPI::~SPI(void)
+{
+}
+
+void SPI::format(int bits, int mode = SPI_MODE_DEFAULT)
+{
+	this->format_bits = bits;
+	this->format_mode = mode;
+
+	this->initSeq |= FORMAT_INIT;
+
+	/* If init sequence is finished */
+	if(initSeq == COMPLETE_INIT)
+	{
+		init();
+	}
+}
+void SPI::frequency(int hz = 1000000)
+{
+	this->frequency_hz = hz;
+
+	this->initSeq |= FREQ_INIT;
+
+	/* If init sequence is finished */
+	if(initSeq == COMPLETE_INIT)
+	{
+		init();
+	}
+}
+
+void SPI::init(void)
+{
+	/* Init SPI parameters */
+	spiParamInit(&params);
+
+	params.speedInHz = frequency_hz;
+	params.bitsPerWord = (format_bits / 8) - 1;
+	params.mode = format_mode;
+	
+	params.sckGpio  = sclk;
+	params.mosiGpio = mosi;
+	params.misoGpio = miso;
+	params.csGpio   = ssel;
+
+	/* Check mapping */
+	if(spiCheckDevice(params.busNum, params.devideId, ONION_SEVERITY_DEBUG_EXTRA) != EXIT_SUCCESS)
+	{
+		std::cout << "[SPI INIT] Error: Spi device is not mapped. Canceling Init." << std::endl;
+		return;
+	}	
+
+	/* Register the device */
+	if(spiRegisterDevice(&params) != EXIT_SUCCESS)
+	{
+		std::cout << "[SPI INIT] Error: Spi device could not be registered. Canceling Init." << std::endl;
+		return;
+	}
+
+	/* Extra setup */
+	if(spiSetupDevice(&params) != EXIT_SUCCESS)
+	{
+		std::cout << "[SPI INIT] Error : Spi device setup failed. Canceling Init." << std::endl;
+		return;
+	}
+}
+
+int SPI::write(int value)
+{
+	if(initSeq != INIT_COMPLETE)
+	{
+		std::cout << "[SPI WRITE] Error: SPI is not initialized." << std::endl;
+		return -1;
+	}
+	/* TODO: Check addr value */
+	uint8_t txBuffer = value;
+	uint8_t rxBuffer;
+	int status;
+	if((status = spiTranfer(&params, &txBuffer, &rxBuffer, 1)) != EXIT_SUCCESS)
+	{
+		std::cout << "[SPI WRITE] Error : SPI write failed, code " << status << "." << std::endl;
+		return -1;
+	}	 
+
+	return rxBuffer;
+}
---
+++ RA8875_Drivers/omegaLib/spi.h
@@ -0,0 +1,45 @@
+/* 
+ * SPI class for Omega2
+ *
+ * Allow compatibility with Mbed code
+ * Not all the functionnalities have been ported,
+ * just the ones used in the project.
+ * 
+ * Author : Alexy Torres Aurora Dugo
+ */
+
+#ifndef __SPI_H_
+#define __SPI_H_
+
+#include "gpio.h"
+#include <onion-spi.h>
+
+#define SPI_MODE_DEFAULT 0
+
+class SPI
+{
+	public:
+		SPI(PinName mosi, PinName miso, PinName sclk, PinName ssel = NC_PIN);
+		virtual ~SPI(void);
+
+		void format(int bits, int mode = SPI_MODE_DEFAULT);
+		void frequency(int hz = 1000000);
+
+		virtual int write(int value);
+
+	private:
+		PinName mosi;
+		PinName miso;
+		PinName sclk;
+		PinName ssel;
+
+		int format_bits;
+		int format_mode;
+
+		int frequency_hz;
+		int initSeq;
+
+		struct spiParams params;
+};
+
+#endif /* __SPI_H_ */
